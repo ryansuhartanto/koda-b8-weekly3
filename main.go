d190894f714df2849e6126d8f8b93c7f73580d59
Ryan Suhartanto <suhartanto@kekkon.nexus>
2026-07-09 18:12:34 +0700

    feat: huh

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"log"
 
+	"charm.land/huh/v2"
 	"github.com/fatih/color"
 )
 
@@ -10,4 +12,26 @@ func main() {
 	fmt.Print("Selamat datang di ")
 	color.New(color.BgBlack).Add(color.FgHiGreen).Add(color.Bold).Print("Wingstop")
 	fmt.Println("!")
+
+	var (
+		exit bool
+	)
+
+	form := huh.NewForm(
+		huh.NewGroup(
+			huh.NewConfirm().
+				Title("Exit?").
+				Value(&exit),
+		),
+	)
+
+	err := form.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if !exit {
+		fmt.Println("?")
+	}
+
 }
